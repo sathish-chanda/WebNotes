b0a9aeaf1e633e476382b6488dccf660b20e911f
sathish-chanda <sathishchandas@gmail.com>
2023-10-02 10:19:33 -0400

    Basic website structure with dummy pages.

---
+++ website/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+def create_app():
+    print("Creating the Flask app - {name}".format(name = __name__))
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hdskfjksdgfoeemfdkfdl lldfjeejfenfejfj'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+    
+    return app
